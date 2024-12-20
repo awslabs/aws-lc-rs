63e846c318325800ff3bc01eaf35b7f07ae217d4
aws-lc-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2024-12-20 15:43:04 +0000

    Collected source files for i686-unknown-linux-gnu

--- aws-lc-sys/builder/cc_builder/i686_unknown_linux_gnu.rs
+++ aws-lc-sys/builder/cc_builder/i686_unknown_linux_gnu.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
-// Wed Dec 11 13:51:29 UTC 2024
+// Fri Dec 20 15:41:03 UTC 2024
 
 use crate::cc_builder::Library;
 

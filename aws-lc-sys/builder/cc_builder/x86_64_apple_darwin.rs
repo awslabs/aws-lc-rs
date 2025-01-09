1b217483874e968a5b95581eb3d2f18978cb0161
aws-lc-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2025-01-09 18:18:00 +0000

    Collected source files from macos-13

--- aws-lc-sys/builder/cc_builder/x86_64_apple_darwin.rs
+++ aws-lc-sys/builder/cc_builder/x86_64_apple_darwin.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
-// Mon Dec 30 19:03:18 UTC 2024
+// Thu Jan  9 18:16:16 UTC 2025
 
 use crate::cc_builder::Library;
 
@@ -108,6 +108,7 @@ pub(super) const CRYPTO_LIBRARY: Library = Library {
         "crypto/evp_extra/p_hmac_asn1.c",
         "crypto/evp_extra/p_kem_asn1.c",
         "crypto/evp_extra/p_methods.c",
+        "crypto/evp_extra/p_pqdsa_asn1.c",
         "crypto/evp_extra/p_rsa_asn1.c",
         "crypto/evp_extra/p_x25519.c",
         "crypto/evp_extra/p_x25519_asn1.c",
@@ -283,10 +284,14 @@ pub(super) const CRYPTO_LIBRARY: Library = Library {
         "third_party/s2n-bignum/x86_att/curve25519/edwards25519_scalarmulbase_alt.S",
         "third_party/s2n-bignum/x86_att/curve25519/edwards25519_scalarmuldouble.S",
         "third_party/s2n-bignum/x86_att/curve25519/edwards25519_scalarmuldouble_alt.S",
+        "third_party/s2n-bignum/x86_att/p256/bignum_montinv_p256.S",
+        "third_party/s2n-bignum/x86_att/p256/p256_montjscalarmul.S",
+        "third_party/s2n-bignum/x86_att/p256/p256_montjscalarmul_alt.S",
         "third_party/s2n-bignum/x86_att/p384/bignum_add_p384.S",
         "third_party/s2n-bignum/x86_att/p384/bignum_deamont_p384.S",
         "third_party/s2n-bignum/x86_att/p384/bignum_deamont_p384_alt.S",
         "third_party/s2n-bignum/x86_att/p384/bignum_littleendian_6.S",
+        "third_party/s2n-bignum/x86_att/p384/bignum_montinv_p384.S",
         "third_party/s2n-bignum/x86_att/p384/bignum_montmul_p384.S",
         "third_party/s2n-bignum/x86_att/p384/bignum_montmul_p384_alt.S",
         "third_party/s2n-bignum/x86_att/p384/bignum_montsqr_p384.S",
@@ -298,8 +303,11 @@ pub(super) const CRYPTO_LIBRARY: Library = Library {
         "third_party/s2n-bignum/x86_att/p384/bignum_tomont_p384_alt.S",
         "third_party/s2n-bignum/x86_att/p384/p384_montjdouble.S",
         "third_party/s2n-bignum/x86_att/p384/p384_montjdouble_alt.S",
+        "third_party/s2n-bignum/x86_att/p384/p384_montjscalarmul.S",
+        "third_party/s2n-bignum/x86_att/p384/p384_montjscalarmul_alt.S",
         "third_party/s2n-bignum/x86_att/p521/bignum_add_p521.S",
         "third_party/s2n-bignum/x86_att/p521/bignum_fromlebytes_p521.S",
+        "third_party/s2n-bignum/x86_att/p521/bignum_inv_p521.S",
         "third_party/s2n-bignum/x86_att/p521/bignum_mul_p521.S",
         "third_party/s2n-bignum/x86_att/p521/bignum_mul_p521_alt.S",
         "third_party/s2n-bignum/x86_att/p521/bignum_neg_p521.S",
@@ -309,5 +317,7 @@ pub(super) const CRYPTO_LIBRARY: Library = Library {
         "third_party/s2n-bignum/x86_att/p521/bignum_tolebytes_p521.S",
         "third_party/s2n-bignum/x86_att/p521/p521_jdouble.S",
         "third_party/s2n-bignum/x86_att/p521/p521_jdouble_alt.S",
+        "third_party/s2n-bignum/x86_att/p521/p521_jscalarmul.S",
+        "third_party/s2n-bignum/x86_att/p521/p521_jscalarmul_alt.S",
     ],
 };
